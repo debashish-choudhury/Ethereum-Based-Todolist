f658728211cf05e7debf27cee92212e6797e65a4
Debashish Choudhury <31796165+deb1398@users.noreply.github.com>
2020-01-24 00:08:03 +0530

    final commit, task added and completed

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -11,6 +11,17 @@ contract TodoList {
 
   mapping(uint => Task) public tasks;
 
+  event TaskCreated(
+    uint id,
+    string content,
+    bool completed
+  );
+
+  event TaskCompleted(
+    uint id,
+    bool completed
+  );
+
   constructor() public {
     createTask("Check out allincloud.co");
   }
@@ -18,6 +29,14 @@ contract TodoList {
   function createTask(string memory _content) public {
     taskCount ++;
     tasks[taskCount] = Task(taskCount, _content, false);
+    emit TaskCreated(taskCount, _content, false);
+  }
+
+  function toggleCompleted(uint _id) public {
+    Task memory _task = tasks[_id];
+    _task.completed = !_task.completed;
+    tasks[_id] = _task;
+    emit TaskCompleted(_id, _task.completed);
   }
 
 }
--- test/TodoList.test.js
+++ test/TodoList.test.js
@@ -17,7 +17,7 @@ contract('TodoList', (accounts) => {
     const taskCount = await this.todoList.taskCount()
     const task = await this.todoList.tasks(taskCount)
     assert.equal(task.id.toNumber(), taskCount.toNumber())
-    assert.equal(task.content, 'Check out dappuniversity.com')
+    assert.equal(task.content, 'Check out allincloud.co')
     assert.equal(task.completed, false)
     assert.equal(taskCount.toNumber(), 1)
   })
